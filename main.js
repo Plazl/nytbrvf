701565d564ddaa3e20cfa206df66b998e63da201
jhnbg rth <I1234itsplaz@outlook.com>
2022-01-28 11:44:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var highScore;
 
 function startGame() {
     myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece.gravity = 0.05;
+    myGamePiece.gravity = 0.3;
     myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myGameArea.start();
 }
